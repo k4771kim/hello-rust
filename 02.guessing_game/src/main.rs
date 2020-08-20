961cca3dec2bf6679016640ebd0bbe0c089948bf
Bogyun Kim <k4771kim@naver.com>
2020-08-20 19:00:14 +0900

    update gitignore

--- 02.guessing_game/src/main.rs
+++ 02.guessing_game/src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("추리 게임");
+    println!("당신이 추측한 숫자를 적으세요");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line (&mut guess).expect("실패");
+
+    println!("당신이 추측한 숫자 {}", guess);
+    
 }
