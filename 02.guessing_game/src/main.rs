24fd9e7a98ee47ba4b9004bf3c0e5b66a25340b4
Bogyun Kim <k4771kim@naver.com>
2020-08-24 17:45:15 +0900

    finish guess game tutorial

--- 02.guessing_game/src/main.rs
+++ 02.guessing_game/src/main.rs
@@ -2,21 +2,31 @@ extern crate rand;
 
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("추리 게임");
-    
     let secret_number = rand::thread_rng().gen_range(1, 101);
+    loop{
+        println!("당신이 추측한 숫자를 적으세요");
 
-    println!("비밀 숫자는 {}", secret_number);
-
-
-    println!("당신이 추측한 숫자를 적으세요");
-
-    let mut guess = String::new();
-
-    io::stdin().read_line (&mut guess).expect("실패");
-
-    println!("당신이 추측한 숫자 {}", guess);
+        let mut guess = String::new();
     
+        io::stdin().read_line (&mut guess).expect("실패");
+    
+        let guess: u32 = guess.trim().parse()
+        .expect("Please type a number!");
+    
+    
+        println!("당신이 추측한 숫자 {}", guess);
+    
+        match guess.cmp(&secret_number) {
+            Ordering::Less    => println!("너무 작아요!"),
+            Ordering::Greater => println!("너무 커요!"),
+            Ordering::Equal   => {
+                println!("당신의 승리입니다!");
+                break;
+            },
+        }
+    }
 }
