a9dbcbfef817842ddbdb57be08fb438398a37626
Bogyun Kim <k4771kim@naver.com>
2020-08-24 15:40:21 +0900

    2-3

--- 02.guessing_game/src/main.rs
+++ 02.guessing_game/src/main.rs
@@ -1,7 +1,16 @@
+extern crate rand;
+
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("추리 게임");
+    
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("비밀 숫자는 {}", secret_number);
+
+
     println!("당신이 추측한 숫자를 적으세요");
 
     let mut guess = String::new();
